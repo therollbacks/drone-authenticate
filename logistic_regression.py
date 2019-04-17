5a230c73993ac29579c1e64d2ea8fa341cbecfa7
rnorfield <rnorfield@gmail.com>
2019-04-16 22:53:10 -0700

    logisitic

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,26 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
+import os, csv
+
+data_list = []
 
 class LogisticRegression:
 
     def __init__(self):
-        data = np.loadtxt('https://utkuufuk.github.io/2018/05/19/binary-logistic-regression/university_admission.txt',
-                          delimiter=",")
+        # with open('./compared_auto/cleandatagp2_009compared.csv') as f:
+        #     next(f)
+        #     for line in csv.reader(f):
+        #         data_list.append([float(line[4]), float(line[5]), float(line[6])])
+        # print(data_list[:5])
+        new_data = np.loadtxt('./compared_auto/cleandatagp2_009compared.csv', delimiter=",")
+        print(new_data[:5])
+        data = np.loadtxt('university_admission.txt', delimiter=",")
         x = data[:, 0:2]
         y = data[:, 2]
+        print('got here')
         print(data[:5])
 
-        fig, ax = plt.subplots()
-        positives = np.where(y == 1)
-        negatives = np.where(y == 0)
-        ax.scatter(x[positives, 0], x[positives, 1], marker="+", c='green')
-        ax.scatter(x[negatives, 0], x[negatives, 1], marker="x", c='red', linewidth=1)
-        plt.title("University Admission", fontsize=16)
-        plt.xlabel("exam 1 score", fontsize=14)
-        plt.ylabel("exam 2 score", fontsize=14)
-        plt.legend(["admitted", "not admitted"])
-        plt.show()
+
+        # fig, ax = plt.subplots()
+        # positives = np.where(y == 1)
+        # negatives = np.where(y == 0)
+        # ax.scatter(x[positives, 0], x[positives, 1], marker="+", c='green')
+        # ax.scatter(x[negatives, 0], x[negatives, 1], marker="x", c='red', linewidth=1)
+        # plt.title("University Admission", fontsize=16)
+        # plt.xlabel("exam 1 score", fontsize=14)
+        # plt.ylabel("exam 2 score", fontsize=14)
+        # plt.legend(["admitted", "not admitted"])
+        # plt.show()
 
         X = np.ones(shape=(x.shape[0], x.shape[1] + 1))
         X[:, 1:] = x
@@ -29,9 +40,9 @@ class LogisticRegression:
         initial_theta = np.zeros(X.shape[1])  # set initial model parameters to zero
         theta = opt.fmin_cg(self.cost, initial_theta, self.cost_gradient, (X, y))
 
-        x_axis = np.array([min(X[:, 1]) - 2, max(X[:, 1]) + 2])
-        y_axis = (-1 / theta[2]) * (theta[1] * x_axis + theta[0])
-        ax.plot(x_axis, y_axis, linewidth=2)
+        # x_axis = np.array([min(X[:, 1]) - 2, max(X[:, 1]) + 2])
+        # y_axis = (-1 / theta[2]) * (theta[1] * x_axis + theta[0])
+        # ax.plot(x_axis, y_axis, linewidth=2)
 
         predictions = np.zeros(len(y))
         predictions[self.sigmoid(X @ theta) >= 0.5] = 1
@@ -49,3 +60,10 @@ class LogisticRegression:
     def cost_gradient(self, theta, X, y):
         predictions = self.sigmoid(X @ theta)
         return X.transpose() @ (predictions - y) / len(y)
+
+def main():
+    mydata = LogisticRegression()
+    print(mydata)
+
+if __name__ == '__main__':
+    main()
